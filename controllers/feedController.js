d4ddab045d6c6a45c597489c1ebfb3a82840d417
minazaher <menazaher115@gmail.com>
2024-02-27 00:39:58 +0200

    integrated with react model

--- controllers/feedController.js
+++ controllers/feedController.js
@@ -1,17 +1,34 @@
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
-        posts:  [
-            {title: "Post Title", content: "This is the first post"}
-            ]
+        posts:
+            [{
+                _id: "1",
+                title: "Post Title",
+                content: "This is the first post",
+                imageUrl: 'images/image.png',
+                creator: {
+                    name: 'Mina'
+                },
+                createdAt: new Date()
+            }]
     })
 }
 
-exports.createPost =  (req, res, next) => {
+exports.createPost = (req, res, next) => {
     const title = req.body.title
     const content = req.body.content
 
     res.status(201).json({
         message: "Successfully Posted!",
-        post: {id: new Date().toISOString(), title: title, content: content}
+        post: {
+            _id: new Date().toISOString(),
+            title: title,
+            content: content,
+            creator: {
+                name: 'Mina'
+            },
+            createdAt: new Date()
+        }
     })
+    console.log("Successfully Posted!")
 }
